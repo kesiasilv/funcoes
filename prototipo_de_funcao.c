d73edf1b93b0ce002949a47e27770e6aff96bc19
Késia Silva <kesiasilvab6@gmail.com>
2023-11-27 19:45:53 -0300

    Create prototipo_de_funcao.c

---
+++ prototipo_de_funcao.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*prototipo de uma função, é usada caso a funcao
+fique depois do main, entao serve para chamar as 
+funçoes que estão depois do main.
+sintaxe:
+<tipoo> <nome_da_funcao>(<parametro>);
+*/
+
+//confira o exemplo:
+
+float maior(float num1, float num2);//prototipo da função
+
+int main(){
+	float x, y, m;
+	
+	printf("digite um numero: \n");
+	scanf("%f", &x);
+	printf("digite outro numero: \n");
+	scanf("%f", &y);
+	
+	m = maior(x, y);
+	
+	printf("Maior: %.2f", m);
+}
+
+float maior(float num1, float num2){//função
+	if(num1 > num2){
+	 	return num1;
+	}else{
+		return num2;
+	}
+}
