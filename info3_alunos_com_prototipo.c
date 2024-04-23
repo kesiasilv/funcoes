8076e0d73326cc6c500205d8cf3f7011be2d329c
Késia Silva <kesiasilvab6@gmail.com>
2024-04-22 21:12:58 -0300

    Update info3_alunos_com_prototipo.c

--- info3_alunos_com_prototipo.c
+++ info3_alunos_com_prototipo.c
@@ -28,23 +28,22 @@ struct Aluno {
 void lerDadosAlunos(struct Aluno Aluno[3]){
 	int i, j;
 
-    for(i = 0; i <3; i++)
-    {
-    printf("==========================\n");
-    printf("Digite o nome do aluno: ");
-	scanf("%50[^\n]s", Aluno[i].nome);
-    fflush(stdin);
+    for(i = 0; i <3; i++){
+    	printf("==========================\n");
+    	printf("Digite o nome do aluno: ");
+    	scanf("%50[^\n]s", Aluno[i].nome);
+    	fflush(stdin);
 
-	printf("Digite a idade do aluno: ");
-	scanf("%d", &Aluno[i].idade);
-    fflush(stdin);
-	
+   	printf("Digite a idade do aluno: ");
+   	scanf("%d", &Aluno[i].idade);
+    	fflush(stdin);
+	    
 	printf("Digite as 3 notas do aluno:\n");
 	for (j = 0; j < 3; j++) {
 	printf("Nota %d: ", j + 1);
 	scanf("%lf", &Aluno[i].notas[j]);
-    fflush(stdin);
-    }
+    	fflush(stdin);
+    	}
     }
 }
 
@@ -52,41 +51,34 @@ void impriDadosAluno(struct Aluno Aluno[3]){
 	int i;
 	
     for(i = 0; i<3; i++){
-    printf("==========================\n");
-    printf("\nInformacoes do Aluno:\n");
+    	printf("==========================\n");
+    	printf("\nInformacoes do Aluno:\n");
 	printf("Nome: %s\n", Aluno[i].nome);
 	printf("Idade: %d\n", Aluno[i].idade);
 	
 	int j;
-  for (j = 0; j < 3; j++)
-        {
-            printf("Notas %d: %.2lf\n", j + 1, Aluno[i].notas[j]);
+  for (j = 0; j < 3; j++){
+       printf("Notas %d: %.2lf\n", j + 1, Aluno[i].notas[j]);
         }
         double soma = 0.0;
         
-        for (j = 0; j < 5; j++)
-        {
+        for (j = 0; j < 5; j++){
             soma+= Aluno[i].notas[j];
         }
         
         double media;
-		media = soma / 3;
+	media = soma / 3;
         printf("Media: %.2lf\n", media);
         
-        if (media >= 7)
-        {
+        if (media >= 7){
             printf("Aluno Aprovado\n");
         }
-        else if ( media >=4 && media < 7)
-        {
+        else if ( media >=4 && media < 7){
             printf("Aluno de recuperacao\n");
         }
-        else if (media < 4)
-        {
+        else{
             printf("Aluno Reprovado\n");
-        }
-        
+        }  
     }
         printf("=======================================\n");
-
 }
